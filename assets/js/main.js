ab6cf1143e21fb66af3a2d149fb1cbae20006bd6
akitsawsbhattarai <57032177+akitsawsbhattarai@users.noreply.github.com>
2022-10-22 19:19:12 +0545

    second update

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,9 @@ if(navToggle){
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if(navClose){
-    navClose.addEventListener('click',()=>{
+    navClose.addEventListener('click',(e)=>{
+        e.preventDefault()
+        console.log ('clickedd...')
         navMenu.classList.remove('show-menu')
     })
 }
@@ -37,26 +39,49 @@ if(navClose){
     }))
 }
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent=document.getElementsByClassName('skills__content'),
-skillsHeader=document.querySelector('.skills__header')
+const skillsContent=document.getElementsByClassName('skills__content')
+console.log(skillsContent)
+skillsHeader=document.getElementsByClassName('skills__header')
 
 function toggleSkills(){
-    let itemClass=this.parentNode.className
+    let itemClass=this.parentNode
+    console.log(itemClass)
 
     for(i=0; i< skillsContent.length; i++){
+        console.log(skillsContent[i])
         skillsContent[i].className='skills__content skills__close'
     }
 
-    if(itemClass=='skills__content skills__close'){
+    if(itemClass.className=='skills__content skills__close'){
         this.parentNode.className='skills__content skills__open'
     }
 }
-skillsHheader.forEach((el)=>{
+Array.from(skillsHeader).forEach((el)=>{
+    // console.log(el)
     el.addEventListener('click', toggleSkills)
+
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tab=document.querySelectorAll( '[data-target]'),
+    tabContents=document.querySelectorAll('[data-content]')
 
+tabContents.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        const target=document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
